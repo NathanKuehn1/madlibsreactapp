e60ac57ad9bb3b373d416499bf1440a6794cafbf
Nathan <linkingitup@outlook.com>
2018-03-19 12:21:58 -0600

    Created madlib component

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Madlibform from './madlib_form';
 
 export default class App extends Component {
   render() {
@@ -11,8 +12,10 @@ export default class App extends Component {
               Fillout the fields below and click the generate button<br/>to see the mad lib story,
               </div>
           </div>
+          <Madlibform />
+          
         </div>
-    </div>
+      </div>
     );
   }
 }
---
+++ src/components/madlib_form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Madlibform extends Component {
+    render() {
+      return <h1>Nathans Mad Libs Form Component </h1>;
+    }
+  }
+
+  export default Madlibform;
